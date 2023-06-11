1efa1af070ccbf12ab30a4821782fef29fdeeb84
Rafael Volpi <volpideveloper@gmail.com>
2023-06-11 20:15:37 -0300

    Js home (#5)
    * Create t
    
    * Create pop-up
    
    * Create img
    
    * Create css
    
    * Delete t
    
    * Add files via upload
    
    * Delete pop-up
    
    * Create imagens-pop
    
    * Delete css
    
    * Delete img
    
    * Create test
    
    * Delete imagens-pop
    
    * Add files via upload
    
    * Delete test
    
    * Create a
    
    * Add files via upload
    
    * Create a
    
    * Add files via upload
    
    * Delete a
    
    * Delete a
    
    * Delete teste directory
    
    * Delete home.html
    
    * Add files via upload
    
    * Create a
    
    * Create a
    
    * Add files via upload
    
    * Delete a
    
    * Add files via upload
    
    * Delete a
    
    * cleaning html sonar bugs - minor
    
    * moving src popup locations
    
    * cleaning up idea files
    
    * Update recuperar.html
    
    * Update principal.html
    
    * Update recuperar.html
    
    * Update perfil.html
    
    * Update login.html
    
    * Update home.html
    
    * Update cadastro.html
    
    * Update cadastro.css
    
    * Update home.css
    
    * Update login.css
    
    * Update recuperar.css
    
    * Update style-principal1.css
    
    * Update style-principal2.css
    
    * Update style-profile1.css
    
    * Update style-profile2.css
    
    ---------
    
    Co-authored-by: ldvt <lucas.vito@latam.com>

---
+++ src/scripts/editar.js
@@ -0,0 +1,16 @@
+      bioEp.init({
+            html: '<div id="bio_ep_content">' +
+            '<img src="img/popup-seta.png" width="54" alt="Participe" style="margin-right: 130px;" />' +
+            '<span></span>' +
+            '<span></span>' +
+            '<span></span>' +
+            '<a href="https://www.instagram.com/" target="_blank" class="bio_btn">Participar do Sorteio &nbsp; <i class="fa fa-instagram"></i></a>' +
+            '</div>',
+            css: '#bio_ep {width: 400px; height: 400px; color: #333; background-color: #fafafa; text-align: center;background-image: url(img/popup-sorteio04.jpg);background-size: cover;}' +
+            '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
+            '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
+            '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
+            '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
+            '.bio_btn {display: inline-block; margin: 18px 0 0 0; padding: 8px 25px 8px 25px; color: #fff; font-size: 19px; font-weight: 900; background-color: #ff4719; border: 1px solid #fff; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 100px; text-decoration: none;}',
+            cookieExp: 0
+        });
